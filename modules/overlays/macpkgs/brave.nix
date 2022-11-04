dced1f371c17cbefe7213b610d06dc5590e9751e
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2022-11-04 12:54:51 +0700

    update brave

--- modules/overlays/macpkgs/brave.nix
+++ modules/overlays/macpkgs/brave.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
     pname = "brave";
-    version = "v1.45.116";
+    version = "v1.45.118";
 
     nativeBuildInputs = [ undmg ];
     src = fetchurl {
         url = "https://github.com/brave/brave-browser/releases/download/${version}/Brave-Browser-universal.dmg";
-        sha256 = "sha256-Df/TgfwWPS+eLWRQzRT74tJSY9V5FYiQe2cvK5UZhXw=";
+        sha256 = "sha256-Q5Bl0925h/mi/K6Il0zhRQ475IG+g1VdY0D41SMN+1w=";
     };
 
     sourceRoot = ".";
